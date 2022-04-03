68e4f3336a487a3b47598d51d15d24553bcb70bb
ExplorerKut <kutbuddin.ambawala@gmail.com>
2022-04-03 10:44:45 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app=Flask(__name__)
+app.config['CUSTOM_MESSAGE']=os.environ.get('CUSTOM_MESSAGE')
+
+@app.route("/")
+def index():
+    return "<h1>"+app.config['CUSTOM_MESSAGE']+"</h1>"
+
+if __name__=="__main___":
+    app.run()
