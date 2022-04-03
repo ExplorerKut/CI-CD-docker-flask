8b1afa308f58fcb4e4ae8d33b572bf82d01d3160
ExplorerKut <kutbuddin.ambawala@gmail.com>
2022-04-03 16:59:51 +0530

    fixed lint errors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config["CUSTOM_MESSAGE"] = os.environ.get("CUSTOM_MESSAGE')
+app.config["CUSTOM_MESSAGE"] = os.environ.get("CUSTOM_MESSAGE")
 
 @app.route("/")
 def index():
