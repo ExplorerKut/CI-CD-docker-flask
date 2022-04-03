f9584d7c7bf8c4404f4af62888bba4db740c116e
ExplorerKut <kutbuddin.ambawala@gmail.com>
2022-04-03 17:04:45 +0530

    fixed lint errors

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
+
 from flask import Flask
 
 app = Flask(__name__)
 app.config["CUSTOM_MESSAGE"] = os.environ.get("CUSTOM_MESSAGE")
 
+
 @app.route("/")
 def index():
     return "<h1>" + app.config["CUSTOM_MESSAGE"] + "</h1>"
+
 
 if __name__ == "__main___":
     app.run()
